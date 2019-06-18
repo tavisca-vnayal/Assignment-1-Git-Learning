2831698e34f8dfa68e535c0a65836854992d627e
tavisca-vnayal <vnayal@tavisca.in>
2019-06-18 22:35:44 +0530

    Normal Print Statement

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 def myFunction() :
     # message to be print on console on calling this function
-    # omiting the -m flag to commit
     print("Welcome to Prejoining Training @ Tavisca")
 
 myFunction()
