8f3f885bd779307fc759fa2b9f830cc1d25b209a
tavisca-vnayal <vnayal@tavisca.in>
2019-06-18 22:29:59 +0530

    Commiting without -m flag

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def myFunction() :
     # message to be print on console on calling this function
+    # omiting the -m flag to commit
     print("Welcome to Prejoining Training @ Tavisca")
 
 myFunction()
