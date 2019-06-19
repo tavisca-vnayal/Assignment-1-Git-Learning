6d5bc78ed73c169092e6c7129777070a16538fdd
tavisca-vnayal <vnayal@tavisca.in>
2019-06-19 14:46:17 +0530

    Hello uses Greeter

--- lib/hello.py
+++ lib/hello.py
@@ -1,11 +1,16 @@
 # Author : Varun Nayal
 # Email  : vnayal@tavisca.in
 
-companyName = "Tavisca"
-programmeName = "Prejoining Training"
+from greeter import greeter
+from greeter import initialize
 
 def myFunction() :
     # message to be print on console on calling this function
-    print("Welcome to " + programmeName + " @ " + companyName)
+    # This is the format of the print statement for greeting new trainees
+    print("Welcome to programmeName @ companyName")
 
 myFunction()
+initialize("Tavisca","Prejoining Training")
+greeter()
+initialize("Tavisca","Onboarding Training")
+greeter()
